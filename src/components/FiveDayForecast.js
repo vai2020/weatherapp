860dd0b679fcb80c1252dde600325a043328dbe7
Student_M5 <student_m5@PS-DAL-M5.lan>
2020-04-09 14:20:10 -0500

    add FiveDayForecast function to the FiveDayForecast.js file

---
+++ src/components/FiveDayForecast.js
@@ -0,0 +1,10 @@
+import React from "react";
+function FiveDayForest() {
+    return(
+        <div>
+            <h1> Five Day Forest Component</h1>
+        </div>
+    );
+};
+
+export default FiveDayForecast;
