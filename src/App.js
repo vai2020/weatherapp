f8d0bef1adba4755fc35b5f3f1352bc36e591080
Student_M5 <student_m5@PS-DAL-M5.lan>
2020-04-09 11:17:56 -0500

    add async getWeatherData

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import './App.css';
+import Axios from 'axios';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {}
+  }
+}
+
+componentDidMount() {
+  this.getWeatherData();
+}
+
+async getWeatherData() {
+  try {
+    const response = await Axios.get("");
   }
 }
 
