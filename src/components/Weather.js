3788713e9515351b0d5b7cc3c852a925a390ba9b
Student_M5 <student_m5@PS-DAL-M5.lan>
2020-04-13 13:05:45 -0500

    add comment/credit on app.js

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,10 +1,1 @@
-import React from "react";
-function Wednesday() {
-    return(
-        <div>
-            <h1> Wednesday Component</h1>
-        </div>
-    );
-};
-
-export default Wednesday;
+i
